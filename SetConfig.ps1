9e88cf33d1dd102f794a4d9205a9ab8a04182330
Brian Hill <bhill@keyfactor.com>
2021-10-25 19:17:41 -0400

    Removed unused files

--- SetConfig.ps1
+++
@@ -1,2 +0,0 @@
-﻿Import-Module "C:\Program Files\Keyfactor\Keyfactor AnyGateway\ConfigurationCmdlets.dll"
-Set-KeyfactorGatewayConfig -LogicalName "DigiCertSym" -FilePath "C:\Program Files\Keyfactor\Keyfactor AnyGateway\Config.json" -PublishAD
